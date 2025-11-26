794b3fbd12516ae60b040cd0b503d71ca3bd74c1
g4briel <guchangge28@gmail.com>
2025-11-25 21:47:25 -0500

    delete: repository

--- src/repositories/user.repository.ts
+++
@@ -1,9 +0,0 @@
-import type { User } from "../models/user.ts";
-
-export interface UserRepository {
-  findById(id: string): Promise<User | null>;
-  findBySession(session: string): Promise<User | null>;
-  create(user: User): Promise<User>;
-  update(user: User): Promise<User>;
-  delete(id: string): Promise<boolean>;
-}
